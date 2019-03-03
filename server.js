0b7aa33588267f08a08e73c9529ba008b569c621
Resti Miranda <restimiranda@Restis-MacBook-Pro.local>
2019-03-02 22:23:58 -0600

    update files

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.engine('handlebars', exphbs({
 app.set('view engine', 'handlebars');
 
 //connecting to MongoDB
-mongoose.connect('mongodb://heroku_jxkjhg1v:6s68tem51mlionrj2sneb7b53c@ds127988.mlab.com:27988/heroku_jxkjhg1v');
+mongoose.connect('');
 
 //mongoose.connect('mongodb://localhost/scraped_news');
 
